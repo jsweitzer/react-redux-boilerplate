a9edffb5240c08f0e6990a2f1c90b0fd91b0256b
Josh <joshsweitzer@gmail.com>
2018-05-30 19:37:13 -0400

    init

---
+++ src/Components/Ingredient.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ingredient extends Component {
+  render() {
+    return (
+      <div className="Ingredient">
+        <header className="Ingredient-header">
+          <h1 className="Ingredient-title">Ingredient</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Ingredient;
